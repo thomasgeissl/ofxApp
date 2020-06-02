a02954d69daa98b65987f3f3c914d6646c9de3c4
Thomas Geissl <thomas.geissl@gmail.com>
2020-06-02 12:58:18 +0200

    added custom title getter, which is used internally

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,17 +1,23 @@
 #include "ofMain.h"
 #include "ofxApp.h"
 
-class ofApp : public ofxApp{
-    public:
-    ofApp(){
+class ofApp : public ofxApp
+{
+public:
+    ofApp()
+        // set window title automatically and dont quite on esc, these are the defaults
+        : ofxApp(true, false)
+    {
         setApplicationName("demo app");
+        // set a custom getter for the window title
+        setGetWindowTitle([this]() { return getApplicationName() + " v" + getApplicationVersion(); });
         setApplicationVersion(1, 0, 0);
         // setApplicationVersion("1.0.0");
     }
-
 };
 
-int main(){
-	ofSetupOpenGL(1024, 768, OF_WINDOW);
-	ofRunApp(new ofApp());
+int main()
+{
+    ofSetupOpenGL(1024, 768, OF_WINDOW);
+    ofRunApp(new ofApp());
 }
--- src/ofxApp.h
+++ src/ofxApp.h
@@ -2,45 +2,67 @@
 
 #include "ofMain.h"
 
-class ofxApp : public ofBaseApp{
+class ofxApp : public ofBaseApp
+{
 public:
-    ofxApp(){
-        ofSetEscapeQuitsApp(false);
-		ofAddListener(ofEvents().update, this, &ofxApp::onUpdate, OF_EVENT_ORDER_BEFORE_APP);
+    ofxApp(bool registerUpdateEvent = true, bool quitOnEscape = false)
+    {
+        ofSetEscapeQuitsApp(quitOnEscape);
+        if (registerUpdateEvent)
+        {
+            ofAddListener(ofEvents().update, this, &ofxApp::onUpdate, OF_EVENT_ORDER_BEFORE_APP);
+        }
+        _getWindowTitle = [this]() { return _applicationName + "[" + _applicationVersion + "] @" + ofToString((int)(ofGetFrameRate())) + "fps"; };
     }
-    void onUpdate(ofEventArgs &e){
-        ofSetWindowTitle(_applicationName +"["+_applicationVersion+"] @"+ofToString((int)(ofGetFrameRate()))+"fps");
+    void onUpdate(ofEventArgs &e)
+    {
+        ofSetWindowTitle(_getWindowTitle());
     }
 
-    std::string getApplicationName(){
+    std::string getApplicationName()
+    {
         return _applicationName;
     }
-    std::string getApplicationVersion(){
+    std::string getApplicationVersion()
+    {
         return _applicationVersion;
     }
-    std::string getOrganizationName(){
+    std::string getOrganizationName()
+    {
         return _organizationName;
     }
-    std::string getOrganizationDomain(){
+    std::string getOrganizationDomain()
+    {
         return _organizationDomain;
     }
-    void setApplicationName(std::string applicationName){
+    void setApplicationName(std::string applicationName)
+    {
         _applicationName = applicationName;
     }
-    void setApplicationVersion(std::string applicationVersion){
+    void setApplicationVersion(std::string applicationVersion)
+    {
         _applicationVersion = applicationVersion;
     }
-    void setApplicationVersion(int major, int minor, int patch){
-        setApplicationVersion(ofToString(major)+"."+ofToString(minor)+"."+ofToString(patch));
+    void setApplicationVersion(int major, int minor, int patch)
+    {
+        setApplicationVersion(ofToString(major) + "." + ofToString(minor) + "." + ofToString(patch));
     }
-    void setOrganizationName(std::string organizationName){
+    void setOrganizationName(std::string organizationName)
+    {
         _organizationName = organizationName;
     }
-    void setOrganizationDomain(std::string organizationDomain){
+    void setOrganizationDomain(std::string organizationDomain)
+    {
         _organizationDomain = organizationDomain;
+    }
+    void setGetWindowTitle(std::function<std::string()> fun)
+    {
+        _getWindowTitle = fun;
     }
     std::string _applicationName;
     std::string _applicationVersion;
     std::string _organizationName;
     std::string _organizationDomain;
+
+    std::function<std::string()> _getWindowTitle;
 };
